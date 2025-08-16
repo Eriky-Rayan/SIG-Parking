ab95d7b5894a056a15b770db2a4c9bd4ad91361c
Eriky-Rayan <eriky.medeiros.706@ufrn.edu.br>
2025-08-16 18:24:11 -0300

    add: tela_equipe

---
+++ equipe.c
@@ -0,0 +1,20 @@
+
+#include <stdio.h>
+#include <stdlib.h>
+void equipe (void){
+    printf("⎸=====================================================⎸\n");
+    printf("⎸                     SIG-Parking                     ⎸\n");
+    printf("⎸=====================================================⎸\n");
+    printf("⎸                  Equipe do Projeto                  ⎸\n");
+    printf("⎸=====================================================⎸\n");
+    printf("⎸ -Eriky Rayan B. De Medeiros                         ⎸\n");
+    printf("⎸ -Email:eriky.medeiros.706@ufrn.edu.br               ⎸\n");
+    printf("⎸ -GitHub:https://github.com/Eriky-Rayan              ⎸\n");
+    printf("⎸=====================================================⎸\n");
+    printf("⎸ -Isaac Vilton Ribeiro                               ⎸\n");
+    printf("⎸ -Email:Isaac.ribeiro.018@ufrn.edu.br                ⎸\n");
+    printf("⎸ -GitHub:https://github.com/Isaac-Ribeiro            ⎸\n");
+    printf("⎸=====================================================⎸\n");
+    printf("⎸ -Git:https://github.com/Eriky-Rayan/SIG-Parking.git ⎸\n");
+    printf("⎸=====================================================⎸\n");
+}
