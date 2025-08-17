e954b59c4880a7a6bc593e6debd6c8b8a2de7abc
Isaac-Ribeiro <isaac.ribeiro.018@ufrn.edu.br>
2025-08-16 22:35:29 -0300

    style: menu_principal

--- SIG-Parking.c
+++ SIG-Parking.c
@@ -23,7 +23,7 @@ void menu_principal(void) {
     printf("⎸ 1 - Módulo Automóveis                               ⎸\n");
     printf("⎸ 2 - Módulo Estacionamentos                          ⎸\n");
     printf("⎸ 3 - Módulo Donos dos Automóveis                     ⎸\n");
-    printf("⎸ 5 - Módulo Equipe do projeto                        ⎸\n");
+    printf("⎸ 5 - Módulo Equipe do Projeto                        ⎸\n");
     printf("⎸ 6 - Sair                                            ⎸\n");
     printf("⎸=====================================================⎸\n");
     printf("\t\t >>Escolha uma opção: ");
