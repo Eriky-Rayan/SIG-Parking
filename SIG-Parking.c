b7a4037660ee1db7f6e5109cd243f0dddda25e04
Isaac-Ribeiro <isaac.ribeiro.018@ufrn.edu.br>
2025-08-16 22:57:51 -0300

    Add: modulo-estacionamentos

--- SIG-Parking.c
+++ SIG-Parking.c
@@ -3,10 +3,12 @@
 #include <stdlib.h>
 
 void menu_principal(void);
+void estacionamentos(void);
 void equipe(void);
 
 int main(void) {
     menu_principal();
+    estacionamentos();
     equipe();
     return 0;
 }
@@ -23,8 +25,8 @@ void menu_principal(void) {
     printf("⎸ 1 - Módulo Automóveis                               ⎸\n");
     printf("⎸ 2 - Módulo Estacionamentos                          ⎸\n");
     printf("⎸ 3 - Módulo Donos dos Automóveis                     ⎸\n");
-    printf("⎸ 5 - Módulo Equipe do Projeto                        ⎸\n");
-    printf("⎸ 6 - Sair                                            ⎸\n");
+    printf("⎸ 4 - Módulo Equipe do Projeto                        ⎸\n");
+    printf("⎸ 0 - Sair                                            ⎸\n");
     printf("⎸=====================================================⎸\n");
     printf("\t\t >>Escolha uma opção: ");
     scanf("%c", &op);
@@ -34,7 +36,31 @@ void menu_principal(void) {
     getchar();
 }
 
-void equipe (void){
+void estacionamentos(void) {
+    char op;
+    system("clear||cls");
+    printf("\n");
+    printf("⎸=====================================================⎸\n");
+    printf("⎸                     SIG-Parking                     ⎸\n");
+    printf("⎸=====================================================⎸\n");
+    printf("⎸                Módulo Estacionamentos               ⎸\n");
+    printf("⎸=====================================================⎸\n");
+    printf("⎸ 1 - Cadastrar Estacionamento                        ⎸\n");
+    printf("⎸ 2 - Exibir dados do Estacionamento                  ⎸\n");
+    printf("⎸ 3 - Alterar dados do Estacionamento                 ⎸\n");
+    printf("⎸ 4 - Excluir Estacionamento                          ⎸\n");
+    printf("⎸ 0 - Voltar ao Menu Principal                        ⎸\n");
+    printf("⎸=====================================================⎸\n");
+    printf("\t\t >>Escolha uma opção: ");
+    scanf("%c", &op);
+    getchar();
+    printf("\n");
+    printf("\t\t >>Aperte qualquer tecla para continuar");
+    getchar();
+}
+
+void equipe (void) {
+    char op;
     system("clear||cls");
     printf("\n");
     printf("⎸=====================================================⎸\n");
