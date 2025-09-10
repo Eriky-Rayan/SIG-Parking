aa41a6ac73cdd6a47eb39abae84663097b6e3015
Isaac-Ribeiro <isaac.ribeiro.018@ufrn.edu.br>
2025-09-10 20:30:44 -0300

    fix: #include "cadastro_vagas.h" no arquivo .c

--- cadastro_vagas.c
+++ cadastro_vagas.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
-#include "veiculos.h"
+#include "cadastro_vagas.h"
 
 //=======================================
 //= Funções do Módulo Cadastro de Vagas =
