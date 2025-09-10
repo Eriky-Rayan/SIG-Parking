14c1fe2f3f382351b6518fd65071c5b24f227a9d
Eriky-Rayan <eriky.medeiros.706@ufrn.edu.br>
2025-09-10 15:51:41 -0300

    modularizarção_veiculos

---
+++ veiculos.h
@@ -0,0 +1,10 @@
+
+//Assinatura das Funções
+//Function Signature
+
+void switch_veiculos(void);
+char veiculos(void);
+void add_veiculos(void);
+void exib_veiculo(void);
+void alterar_veiculo(void);
+void exclu_veiculo(void);
