c8fd672f313154a14f9f61d6d90a97de5dfa45fd
Isaac-Ribeiro <isaac.ribeiro.018@ufrn.edu.br>
2025-09-10 20:18:30 -0300

    add: modularização cadastro_vagas

---
+++ cadastro_vagas.h
@@ -0,0 +1,10 @@
+
+//Assinatura das Funções
+//Function Signature
+
+void switch_cadastro_vagas(void);
+char cadastro_vagas(void);
+void add_cadastro_vagas(void);
+void exib_cadastro_vagas(void);
+void alterar_cadastro_vagas(void);
+void exclu_cadastro_vagas(void);
