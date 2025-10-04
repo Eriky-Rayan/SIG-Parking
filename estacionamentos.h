d54e8fc0939d209c2ae550cf50bb2d37341159d8
Isaac-Ribeiro <isaac.ribeiro.018@ufrn.edu.br>
2025-10-03 21:39:23 -0300

    add: struct no módulo estacionamentos

--- estacionamentos.h
+++ estacionamentos.h
@@ -8,3 +8,12 @@ void add_estacionamentos(void);
 void exib_estacionamentos(void);
 void alterar_estacionamentos(void);
 void exclu_estacionamentos(void);
+
+struct estacionamentos
+{
+    char n_estaci[8];
+    char n_estaci_lida[8];
+    char placa[12];
+};
+
+typedef struct estacionamentos Estacionamentos;
