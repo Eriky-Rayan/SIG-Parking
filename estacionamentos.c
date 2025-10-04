8240a68157ad390d5df94957b4940d41ae3ff7c9
Isaac-Ribeiro <isaac.ribeiro.018@ufrn.edu.br>
2025-10-03 21:41:56 -0300

    fix: struct no módulo estacionamentos

--- estacionamentos.c
+++ estacionamentos.c
@@ -4,6 +4,8 @@
 #include <string.h>
 #include "estacionamentos.h"
 
+typedef struct estacionamentos Estacionamentos;
+
 //=====================================
 //= Funções do Módulo Estacionamentos =
 //=====================================
