ebc78038c1162e8182076d2fd0321c12fe98ee1d
Isaac-Ribeiro <isaac.ribeiro.018@ufrn.edu.br>
2025-10-19 17:21:29 -0300

    feat: adição .h e .c do módulo admin

---
+++ admin.h
@@ -0,0 +1,10 @@
+//Assinatura das Funções
+//Function Signature
+
+void switch_admin(void);
+char admin(void);
+void exclu_veiculo(void);
+void exclu_estacionamentos(void);
+void exclu_dono_veiculo(void);
+void exclu_cadastro_vagas(void);
+
